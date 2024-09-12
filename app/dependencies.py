35fcfda8a1463ab0eb28a9b7db81f76b4f956c87
xiaohuo-remote-ubuntu <xiaohuo-desu@outlook.com>
2024-09-13 00:15:32 +0800

    init

---
+++ app/dependencies.py
@@ -0,0 +1,8 @@
+from fastapi import Header, HTTPException
+
+"""
+The example of custom module
+"""
+async def get_token_header(x_token: str = Header()):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
